7bbe1e660b526ec1e76fcf83e390e4a1155dd43d
YoungHypo <haibo942us@gmail.com>
2025-06-20 00:19:31 -0700

    add DISABLE_METAL in example/cpp CmakeLists.txt
    add DISABLE_METAL in example/cpp CmakeLists.txt

--- examples/cpp/build.sh
+++ examples/cpp/build.sh
@@ -3,8 +3,6 @@ cd build
 cmake ..
 make
 
-# Create Metal library symlink only if not in CI environment
-if [[ "$CI" != "true" && "$GITHUB_ACTIONS" != "true" ]]; then
-    ln -sf ../../../cactus/ggml-llama.metallib default.metallib
-    echo "Created Metal library symlink"
-fi
+ln -sf ../../../cactus/ggml-llama.metallib default.metallib
+
+./cactus_vlm_ffi
